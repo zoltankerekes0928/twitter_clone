ed13bcd4694bf2b6902f71e9a319db3fd14abc73
Zoltan Kerekes <zoltankerekes0928@gmail.com>
2024-12-29 09:20:16 +0100

    14-posts/likes section updated by adding feedtype and endpoints

--- client/src/components/common/Posts.jsx
+++ client/src/components/common/Posts.jsx
@@ -3,13 +3,17 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery} from "@tanstack/react-query"
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
     const getPostEndpoint = () => {
     switch (feedType) {
       case "forYou":
         return "/api/posts/all";
       case "following":
         return "/api/posts/following";
+      case "posts":
+        return `/api/posts/user/${username}`;
+      case "likes":
+        return `/api/posts/likes/${userId}`;
       default:
         return "/api/posts/all";
     }
@@ -30,16 +34,15 @@ const Posts = ({ feedType }) => {
 			return data
 		}
 		catch(error){
-			console.log(error);
-			throw new Error(error)
+			console.log(error.message);
+			throw new Error(error.message)
 		}
 		}
 	})
 
-
 useEffect(()=>{
 	refetch()
-}, [feedType, refetch])
+}, [feedType, username, refetch])
 
   return (
     <>
